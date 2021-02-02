74cb1b23f088f7ed7275a0c3e4613fac2aa9b3c6
perih <perih@kotatko.com>
2021-02-02 13:43:49 -0800

    Add project files.

---
+++ MinMessage.Shared/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MinMessage
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    public async void OnClick (object sender, RoutedEventArgs e)
+    {
+      MessageDialog dialog = new MessageDialog("Back clicked");
+      dialog.Commands.Add(new UICommand("ok", null));
+      var cmd = await dialog.ShowAsync();
+
+      if (cmd.Label == "Yes")
+      {
+        // do something
+      }
+    }
+  }
+}
